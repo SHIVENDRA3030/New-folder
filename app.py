db419e9174a34fb346c3f2c19e5dbd94bf8beb97
Shivendra kumar <shivendra3030kumar@gmail.com>
2025-01-19 17:58:58 +0530

    A

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 from pymongo import MongoClient
-import json
 
 app = Flask(__name__)
 
-# MongoDB setup
-client = MongoClient("mongodb://localhost:27017/")
-db = client.movieDB  # Your database name
-movies_collection = db.movies  # Your collection name
+# Connect to MongoDB server
+client = MongoClient('mongodb://localhost:27017/')
+
+# Create or access the 'movieDB' database
+db = client['movieDB']
+
+# Create or access the 'movies' collection
+collection = db['movies']
 
 @app.route('/')
-def home():
-    page = int(request.args.get('page', 1))  # Page number
-    movies_per_page = 10
-    movies = list(movies_collection.find().sort('upload_date', -1).skip((page - 1) * movies_per_page).limit(movies_per_page))
+def index():
+    movies = collection.find()  # Fetch all movies from MongoDB
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/<int:id>')
+def movie_details(id):
+    # Fetch the movie details by ID
+    movie = collection.find_one({"id": id})  # Find the movie by its unique 'id'
     
-    return render_template('index.html', movies=movies, page=page)
-
-@app.route('/movie/<movie_id>')
-def movie_details(movie_id):
-    movie = movies_collection.find_one({'_id': movie_id})
+    if movie is None:
+        return "Movie not found", 404  # Return a 404 if movie is not found
+    
     return render_template('movie_details.html', movie=movie)
-
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    results = list(movies_collection.find({"title": {"$regex": query, "$options": "i"}}))
-    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- mongo.py
+++ mongo.py
@@ -1,29 +1,77 @@
 from pymongo import MongoClient
 
-# Connect to MongoDB server (assuming it's running on localhost and default port 27017)
+# Connect to MongoDB server
 client = MongoClient('mongodb://localhost:27017/')
 
-# Create a new database (if it doesn't exist, MongoDB will create it when you first use it)
-db = client['movieDB']  # 'movieDB' is the name of your new database
+# Create or access the 'movieDB' database
+db = client['movieDB']
 
-# Create a new collection (if it doesn't exist, MongoDB will create it when you first use it)
-collection = db['movies']  # 'movies' is the name of your collection
+# Create or access the 'movies' collection
+collection = db['movies']
 
-# Insert a sample document (you can customize this to match your movie data)
-movie_data = {
-    'title': 'Inception',
-    'description': 'A mind-bending thriller about dream manipulation.',
-    'upload_date': '2025-01-19',
-    'image_url': 'https://example.com/inception.jpg',
-    'watch_links': ['https://example.com/watch/inception'],
-    'download_links': {
-        '480p': 'https://example.com/download/inception_480p',
-        '720p': 'https://example.com/download/inception_720p',
-        '1080p': 'https://example.com/download/inception_1080p',
+# Movie data to be inserted
+movies_data = [
+    {
+        "id": 58,
+        "title": "Yennai Arindhaal (2015)",
+        "links": [
+            {"name": "Yennai Arindhaal 2015 1080p AMZN WEB-DL DDP5.1 H.264-PMI.mkv [11.04 GB]", "url": "https://new.gdflix.dad/file/fIwuihcZtw"},
+            {"name": "Yennai Arindhaal 2015 720p AMZN WEB-DL DDP5.1 H.264-PMI.mkv [6.03 GB]", "url": "https://new.gdflix.dad/file/cS2GaYHMW8"},
+            {"name": "Yennai Arindhaal 2015 1080p AMZN WEB-DL DDP5.1 H.265-PMI.mkv [4.95 GB]", "url": "https://new.gdflix.dad/file/oey9VTyNDt"},
+            {"name": "Yennai Arindhaal 2015 720p AMZN WEB-DL DDP5.1 H.265-PMI.mkv [1.8 GB]", "url": "https://new.gdflix.dad/file/tvSuZ1hFRD"}
+        ]
+    },
+    {
+        "id": 57,
+        "title": "The Calendar Killer (2025)",
+        "links": [
+            {"name": "The Calendar Killer 2025 1080p AMZN WEB-DL DDP5.1 HEVC - Saon [2.82 GB]", "url": "https://new3.filepress.top/file/67891ce086bd132cd0b7aa54"},
+            {"name": "The Calendar Killer 2025 1080p AMZN WEB-DL DDP5.1 HEVC - Saon [2.82 GB]", "url": "https://new10.gdtot.dad/file/3026040725"}
+        ]
+    },
+    {
+        "id": 56,
+        "title": "Flower Crew: Joseon Marriage Agency S01",
+        "links": [
+            {"name": "Flower Crew Joseon Marriage Agency S01 1080p AMZN WEB-DL DDP2.0 HEVC - Saon [28.38 GB]", "url": "https://new.gdflix.dad/file/qaLWMDWh9D"},
+            {"name": "Flower Crew Joseon Marriage Agency S01 1080p AMZN WEB-DL DDP2.0 HEVC - Saon [28.38 GB]", "url": "https://new10.gdtot.dad/file/30474793191"}
+        ]
+    },
+    {
+        "id": 54,
+        "title": "XO, Kitty Season 1",
+        "links": [
+            {"name": "XO Kitty S01 1080p NF WEB-DL DDP5.1 Atmos HEVC - Saon [7.47 GB]", "url": "https://new.gdflix.dad/file/1otGzX4c2q"},
+            {"name": "XO Kitty S01 1080p NF WEB-DL DDP5.1 Atmos HEVC - Saon [7.47 GB]", "url": "https://new10.gdtot.dad/file/8019219291"}
+        ]
+    },
+    {
+        "id": 55,
+        "title": "XO, Kitty Season 2",
+        "links": [
+            {"name": "XO Kitty S02 1080p NF WEB-DL DDP5.1 Atmos HEVC - Saon [6.19 GB]", "url": "https://new.gdflix.dad/file/OiuOA7au8O"},
+            {"name": "XO Kitty S02 1080p NF WEB-DL DDP5.1 Atmos HEVC - Saon [6.19 GB]", "url": "https://new10.gdtot.dad/file/6649449091"}
+        ]
+    },
+    {
+        "id": 52,
+        "title": "XO, Kitty Season 1",
+        "links": [
+            {"name": "XO Kitty S01 1080p NF WEB-DL AAC5.1 AV1 - Saon [3.34 GB]", "url": "https://new.gdflix.dad/file/BmCNTGWhiW"},
+            {"name": "XO Kitty S01 1080p NF WEB-DL AAC5.1 AV1 - Saon [3.34 GB]", "url": "https://new10.gdtot.dad/file/3587905950"}
+        ]
+    },
+    {
+        "id": 53,
+        "title": "XO, Kitty Season 2",
+        "links": [
+            {"name": "XO Kitty S02 1080p NF WEB-DL AAC5.1 AV1 - Saon [2.98 GB]", "url": "https://new.gdflix.dad/file/Ugd7HfNRhQ"},
+            {"name": "XO Kitty S02 1080p NF WEB-DL AAC5.1 AV1 - Saon [2.98 GB]", "url": "https://new10.gdtot.dad/file/3196057176"}
+        ]
     }
-}
+]
 
-# Insert the movie data into the 'movies' collection
-collection.insert_one(movie_data)
+# Insert the data into the 'movies' collection
+collection.insert_many(movies_data)
 
-print("Database and collection created successfully, and data inserted!")
+print("Data inserted successfully!")
